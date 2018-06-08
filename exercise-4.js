d11900d8e1c1490aa07cac749ae966f43f713307
kod2nd <colinongengkiat@gmail.com>
2018-06-08 15:52:37 +0800

    Exercise 4 and 5

--- exercise-4.js
+++ exercise-4.js
@@ -1,43 +1,73 @@
 // 1. Create an array `names` with at least 3 elements: `'david'`, `'gordon'`, and
 //    `'sahil'` and log it to the console
 
+let names = ['david', 'gordon', 'sahil']
+console.log(names)
 
 // 2. Log the value of the third element in `names` to the console
 
+console.log(names[2])
+
 
 // 3. Log the value of the `names` array's `length` property to the console
-
+console.log(names.length)
 
 // 4. Set the value of the `length` property to `1`
 
+names.length = 1
 
 // 6. Once again, log the value of the `names` array's `length` property to the
 //    console. Did it change?
 
+console.log(names.length)
+
 
 // 7. Log the entire `names` array to the console again
 
+console.log(names)
 
 // 8. Add a few more elements to the array using `push`
+
+names.push("Stuff", 41235, 123141)
 
 
 // 9. Log the last element of the array to the console
 
+console.log(names[names.length - 1])
+
 
 // 10. Log the second last element of the array to the console
+
+console.log(names[names.length - 2])
 
 
 // 11. Change the `textContent` property of the _second_ `p` tag on the page to
 //    `"JavaScript was here!"`
 
+let pArray = document.querySelectorAll("p")
+console.log(pArray)
+
+// document.querySelectorAll("p")[1].textContent = "JavaSript was here!"
+
 
 // 12. Convert the `names` array to a string, with each element separated by a
 //    '<br>'. Store it in a new variable called `namesString` e.g. if `names` is
 //    `['harry', 'ron', 'dean']` then `namesString` will be `"harry<br>ron<br>dean"`
-
+const namesString = names.join('<br>')
+console.log(namesString);
 
 // 13. Change the `innerHTML` property of the _first_ `p` tag on the page to
 //    `namesString`. What happens when you use `textContent` instead of
 //    `innerHTML`?
+
+const pTagArray = document.querySelectorAll('p');
+
+console.log(pTagArray[0])
+
+console.log(pTagArray[0].textContent)
+
+pTagArray[0].innerHTML = namesString
+
+// pTagArray[0].textContent = namesString
 
 
--- exercise-5.js
+++ exercise-5.js
@@ -1,17 +1,96 @@
 // 1. Create a new array `shoppingList` with a few string elements
+const shoppingList = ['Bread', 'Cooking Oil', 'Frying Pan']
 
 
 // 2. Using `forEach`, log every element of the `shoppingList` array to the console
+
+shoppingList.forEach((element) => { console.log(element) });
 
 
 // 3. For each element in the `shoppingList` array, create an `<li>` node and
 //    append it to the `<ul>` element on the page (use `document.createElement`
 //    and `.appendChild`)
 
+// Method 1:
+
+shoppingList.forEach((element) => {
+
+    // create <li> Tag
+    const createLiTag = () => {
+        let tagLi = document.createElement('li');
+        tagLi.textContent = element;
+        return tagLi
+    }
+
+    // add the Li to the Ul
+    const addLiTag = () => {
+        return document.querySelector('ul').appendChild(createLiTag())
+    }
+
+    // Call the Add <li> Tag function!!!!!
+    addLiTag()
+
+})
+
+// Method 2
+shoppingList.forEach((element, index) => {
+
+    // create <li> Tag
+    const createLiTag = () => {
+        return document.createElement('li');
+    }
+
+    // add the Li to the Ul
+    const addLiTag = () => {
+        return document.querySelector('ul').appendChild(createLiTag())
+    }
+
+    // Call the Add <li> Tag function!!!!!
+    addLiTag()
+
+    // Add the shopping list elements to the created lists
+    document.querySelector('ul').querySelectorAll('li')[index + 3].textContent = element
+})
+
+// Method 3 - Its better to create the function outside then call it in the loop
+
+// create <li> Tag
+const createLiTag = (element) => {
+    let tagLi = document.createElement('li');
+    tagLi.textContent = element;
+    return tagLi
+}
+
+// add the Li to the Ul
+const addLiTag = (element) => {
+    return document.querySelector('ul').appendChild(createLiTag(element))
+}
+
+// Loop through shopping list
+shoppingList.forEach((element) => {
+    // Call the createLiTag
+    createLiTag(element)
+    // Call the Add <li> Tag function!!!!!
+    addLiTag(element)
+})
+
+
+
+
 
 // 4. Create a new array `numbers` with a few numeric elements
+
+const numbers = [1, 232, 31, 315, -123, -2512]
 
 
 // 5. Using `map` on the `numbers` array, create a new array called
 //    `squareNumbers` where each element is the square of the corresponding
 //    element in the `numbers` array
+
+const fuctionThatSquaresNumbers = (num) => {
+    return num ** 2;
+}
+
+const squareNumbers = numbers.map(fuctionThatSquaresNumbers);
+
+console.log(squareNumbers);
